e9dbdeb63627822126f265848d0311dc041710e4
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-04-18 08:00:30 +0000

    db:migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_095340) do
+ActiveRecord::Schema.define(version: 2019_04_18_074524) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
